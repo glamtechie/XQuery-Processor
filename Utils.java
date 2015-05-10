5aa220a6df9681151bb7fbb92aaf6046e12432ff
Srishty <srishtyagrawal@eng.ucsd.edu>
2015-05-10 16:44:41 -0700

    Utils.java

--- Utils.java
+++ Utils.java
@@ -22,9 +22,11 @@ public class Utils {
     return new ArrayList<Node>(new LinkedHashSet<Node>(nodes));
     }
 
-  public static void preorderTraversal(Node root, ArrayList<String> pre)       
+  public static void preorderTraversal(Node root, ArrayList<String> pre,int tabs, int maxtab)       
   {
-	
+	String tab = "";
+	for (int k=0;k <tabs; k++)
+		tab = tab+"\t";		
 	String nodename = "";
         if(root==null)
 		return; 
@@ -32,16 +34,24 @@ public class Utils {
 	{
 
 		nodename = root.getNodeName();
-        	pre.add("<"+nodename+">");
+		pre.add("\n");
+        	pre.add(tab+"<"+nodename+">");
         	NodeList children = root.getChildNodes();
         	for (int i = 0; i < children.getLength(); i++) 
 		{
 			if (children.item(i) instanceof Text)
 				pre.add(children.item(i).getTextContent());
 			else   
-        			preorderTraversal(children.item(i), pre);      
+			{
+	 			maxtab = tabs+1;	
+				preorderTraversal(children.item(i), pre, tabs+1, maxtab);      
+			}
 		}
-        	pre.add("</"+nodename+">");
+		if (tabs < maxtab) 
+        	{
+			pre.add("\n"+tab);
+		}
+		pre.add("</"+nodename+">");
         	return;
    	}
 	else if (root instanceof Text) 
@@ -53,9 +63,10 @@ public class Utils {
   public static void printInDocOrder(ArrayList<Node> nodes) 
   {
 	ArrayList<String> pre = new ArrayList<String>();
+	System.out.format("Size of the result %d",nodes.size());
 	for(int i=0;i<nodes.size();i++)
 	{	
-		preorderTraversal(nodes.get(i), pre);
+		preorderTraversal(nodes.get(i), pre, 0,0);
 		for(int j=0;j<pre.size();j++)
 		{
 			System.out.format("%s",pre.get(j));		
